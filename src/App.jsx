b6e81c449c90b3bb9722cc6d95cb283f50271536
GrahamQuan <grahamquan24@gmail.com>
2023-09-10 09:41:55 +0800

    feat: react-query devtool

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+
 import HomePage from './pages/home'
 import TraditionalHeroesPage from './pages/traditional-heroes'
 import RQHeroesPage from './pages/rq-heroes'
 import './App.css'
 
+const queryClient = new QueryClient()
+
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/super-heroes">Traditional Super Heroes</Link>
-            </li>
-            <li>
-              <Link to="/rq-super-heroes">RQ Super Heroes</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/super-heroes" element={<TraditionalHeroesPage />} />
-          <Route path="/rq-super-heroes" element={<RQHeroesPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/super-heroes">Traditional Super Heroes</Link>
+              </li>
+              <li>
+                <Link to="/rq-super-heroes">RQ Super Heroes</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/super-heroes" element={<TraditionalHeroesPage />} />
+            <Route path="/rq-super-heroes" element={<RQHeroesPage />} />
+          </Routes>
+        </div>
+      </Router>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+    </QueryClientProvider>
   )
 }
 
